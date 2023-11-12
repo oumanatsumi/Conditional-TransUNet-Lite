89fd0b0ab16501e408b7383803d57c9ae90c32f7
OumaNatsumi <oumanatsumi@126.com>
2023-11-12 15:53:26 +0800

    classic TransUNet

--- train.py
+++ train.py
@@ -8,8 +8,6 @@ import torch.backends.cudnn as cudnn
 from networks.vit_seg_modeling import VisionTransformer as ViT_seg
 from networks.vit_seg_modeling import CONFIGS as CONFIGS_ViT_seg
 from trainer import trainer_synapse
-import netron
-from torch import onnx as onnx
 from tensorboardX import SummaryWriter as SummaryWriter
 from torchinfo import summary
 
