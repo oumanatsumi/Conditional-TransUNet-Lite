0f3b09824d440eee1c6c99f6d6e124347437f044
黄一骏（21软） <512717388@qq.com>
2023-11-16 20:30:19 +0800

    231116 update focal loss,wce

--- trainer.py
+++ trainer.py
@@ -53,7 +53,7 @@ def trainer_synapse(args, model, snapshot_path):
     beta = 0.9
     wce_weight = torch.from_numpy(np.array([200000/700, 1])).float().cuda()
     ce_loss = CrossEntropyLoss()
-    # wce_loss = CrossEntropyLoss(wce_weight)
+    wce_loss = CrossEntropyLoss(wce_weight)
     dice_loss = DiceLoss(num_classes)
     focal_loss = FocalLoss(weight=wce_weight)
     optimizer = optim.SGD(model.parameters(), lr=base_lr, momentum=0.9, weight_decay=0.0001)
@@ -77,10 +77,11 @@ def trainer_synapse(args, model, snapshot_path):
             outputs = model(image_batch)
             # loss_itm = ce_loss(ITM_logits, ITM_labels)
             loss_ce = ce_loss(outputs, label_batch[:].long())
-            # loss_wce = wce_loss(outputs, label_batch[:].long())
+            loss_wce = wce_loss(outputs, label_batch[:].long())
+            loss_focal = focal_loss(outputs, label_batch[:].long())
             loss_dice = dice_loss(outputs, label_batch, softmax=True)
-            # loss = alpha * loss_ce + (1-alpha) * loss_dice
-            loss = loss_ce
+            loss = alpha * loss_focal + (1-alpha) * loss_dice
+            # loss = loss_ce
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -92,11 +93,14 @@ def trainer_synapse(args, model, snapshot_path):
             writer.add_scalar('info/lr', lr_, iter_num)
             writer.add_scalar('info/train_total_loss', loss, iter_num)
             writer.add_scalar('info/train_loss_ce', loss_ce, iter_num)
+            writer.add_scalar('info/train_loss_wce', loss_wce, iter_num)
+            writer.add_scalar('info/train_loss_focal', loss_focal, iter_num)
             writer.add_scalar('info/train_loss_dice', loss_dice, iter_num)
+
 
             # logging.info('iteration %d : train_loss : %f, train_loss_ce: %f, train_loss_dice: %f' % (iter_num, loss.item(), loss_ce.item(), loss_dice.item()))
 
-            if iter_num % 1 == 0:
+            if iter_num % 200 == 0:
                 image = image_batch[1, 0:1, :, :]
                 image = (image - image.min()) / (image.max() - image.min())
                 writer.add_image('train/Image', image, iter_num)
@@ -110,6 +114,8 @@ def trainer_synapse(args, model, snapshot_path):
             hd95_cnt = 0
             valid_loss = 0.0
             valid_loss_ce = 0.0
+            valid_loss_wce = 0.0
+            valid_loss_focal = 0.0
             valid_loss_dice = 0.0
             valid_mean_hd95 = 0.0
             for i_valid_batch, sampled_valid_batch in enumerate(validloader):
@@ -121,9 +127,11 @@ def trainer_synapse(args, model, snapshot_path):
                 model.transformer.embeddings.feature_embeddings = nn.Parameter(torch.Tensor(batch_embedded_feature).cuda())
                 outputs = model(valid_image_batch)
                 valid_loss_ce += ce_loss(outputs, valid_label_batch[:].long()).item()
+                valid_loss_wce += wce_loss(outputs, valid_label_batch[:].long()).item()
+                valid_loss_focal += focal_loss(outputs, valid_label_batch[:].long()).item()
                 valid_loss_dice += dice_loss(outputs, valid_label_batch, softmax=True).item()
-                # valid_loss += alpha * valid_loss_ce + (1-alpha) * valid_loss_dice.item()
-                valid_loss = valid_loss_ce
+                valid_loss += alpha * valid_loss_focal + (1-alpha) * valid_loss_dice
+                # valid_loss = valid_loss_ce
                 ot = torch.argmax(torch.softmax(outputs, dim=1), dim=1, keepdim=False).cpu().detach().numpy()
                 vl = valid_label_batch.cpu().detach().numpy()
                 if ot.any() and vl.any():
@@ -131,6 +139,8 @@ def trainer_synapse(args, model, snapshot_path):
                     hd95_cnt = hd95_cnt + 1
             valid_loss /= valid_cnt
             valid_loss_ce /= valid_cnt
+            valid_loss_wce /= valid_cnt
+            valid_loss_focal /= valid_cnt
             valid_loss_dice /= valid_cnt
             if hd95_cnt == 0:
                 valid_mean_hd95 = float('inf')
@@ -138,11 +148,15 @@ def trainer_synapse(args, model, snapshot_path):
                 valid_mean_hd95 /= hd95_cnt
             writer.add_scalar('info/valid_total_loss', valid_loss, iter_num)
             writer.add_scalar('info/valid_loss_ce', valid_loss_ce, iter_num)
+            writer.add_scalar('info/valid_loss_wce', valid_loss_wce, iter_num)
+            writer.add_scalar('info/valid_loss_focal', valid_loss_focal, iter_num)
             writer.add_scalar('info/valid_loss_dice', valid_loss_dice, iter_num)
-            logging.info('iteration %d : train_loss : %f, train_loss_ce: %f, train_loss_dice: %f valid_loss : %f, valid_loss_ce: %f, valid_loss_dice: %f, valid_mean_hd95: %f'
-                         % (iter_num, loss.item(), loss_ce.item(), loss_dice.item(), valid_loss, valid_loss_ce, valid_loss_dice, valid_mean_hd95))
+            logging.info('iteration %d : train_loss : %f, train_loss_ce: %f, train_loss_wce: %f,train_loss_focal: %f, train_loss_dice: %f '
+                         'valid_loss : %f, valid_loss_ce: %f, valid_loss_wce : %f, valid_loss_focal: %f, valid_loss_dice: %f, valid_mean_hd95: %f'
+                         % (iter_num, loss.item(), loss_ce.item(),loss_wce.item(),loss_focal.item(), loss_dice.item(),
+                            valid_loss, valid_loss_ce, valid_loss_wce, valid_loss_focal, valid_loss_dice, valid_mean_hd95))
 
-        save_interval = 20  # int(max_epoch/6)
+        save_interval = 40  # int(max_epoch/6)
         if epoch_num > int(max_epoch / 2) and (epoch_num + 1) % save_interval == 0:
         # if (epoch_num + 1) % save_interval == 0:
             save_mode_path = os.path.join(snapshot_path, 'epoch_' + str(epoch_num) + '.pth')
@@ -159,6 +173,8 @@ def trainer_synapse(args, model, snapshot_path):
             hd95_cnt = 0
             test_loss = 0.0
             test_loss_ce = 0.0
+            test_loss_wce = 0.0
+            test_loss_focal = 0.0
             test_loss_dice = 0.0
             test_mean_hd95 = 0.0
             for i_test_batch, sampled_test_batch in enumerate(testloader):
@@ -171,8 +187,10 @@ def trainer_synapse(args, model, snapshot_path):
                     torch.Tensor(batch_embedded_feature).cuda())
                 outputs = model(test_image_batch)
                 test_loss_ce += ce_loss(outputs, test_label_batch[:].long()).item()
+                test_loss_wce += wce_loss(outputs, test_label_batch[:].long()).item()
+                test_loss_focal += focal_loss(outputs, test_label_batch[:].long()).item()
                 test_loss_dice += dice_loss(outputs, test_label_batch, softmax=True).item()
-                test_loss += alpha * test_loss_ce + (1-alpha) * test_loss_dice.item()
+                test_loss += alpha * test_loss_focal + (1-alpha) * test_loss_dice
                 ot = torch.argmax(torch.softmax(outputs, dim=1), dim=1, keepdim=False).cpu().detach().numpy()
                 vl = test_label_batch.cpu().detach().numpy()
                 if ot.any() and vl.any():
@@ -180,6 +198,8 @@ def trainer_synapse(args, model, snapshot_path):
                     hd95_cnt = hd95_cnt + 1
             test_loss /= test_cnt
             test_loss_ce /= test_cnt
+            test_loss_wce /= test_cnt
+            test_loss_focal /= test_cnt
             test_loss_dice /= test_cnt
             if hd95_cnt == 0:
                 test_mean_hd95 = float('inf')
@@ -187,10 +207,13 @@ def trainer_synapse(args, model, snapshot_path):
                 test_mean_hd95 /= test_cnt
             writer.add_scalar('info/test_total_loss', test_loss, iter_num)
             writer.add_scalar('info/test_loss_ce', test_loss_ce, iter_num)
+            writer.add_scalar('info/test_loss_wce', test_loss_wce, iter_num)
+            writer.add_scalar('info/test_loss_focal', test_loss_dice, iter_num)
             writer.add_scalar('info/test_loss_dice', test_loss_dice, iter_num)
+
             logging.info(
-                'TEST RESULT : test_loss : %f, test_loss_ce: %f, test_loss_dice: %f'
-                % (test_loss, test_loss_ce, test_loss_dice))
+                'TEST RESULT : test_loss : %f,  test_loss_ce: %f, test_loss_wce: %f,test_loss_focal: %f ,test_loss_dice: %f'
+                % (test_loss, test_loss_ce,test_loss_wce, test_loss_focal, test_loss_dice))
             logging.info(
                 'DSC :  %f, hd95: %f,'
                 % (1-test_loss_dice, test_mean_hd95))
